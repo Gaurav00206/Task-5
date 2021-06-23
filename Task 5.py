45dc94ced7d10708c982bd45538b611bb271bee2
Gaurav Mishra <67703653+Gaurav00206@users.noreply.github.com>
2021-06-23 09:57:44 +0530

    Add files via upload

---
+++ Task 5.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+list1 = [12, -7, 5, 64, -14]
+
+for number in list1 :
+    if number > 0:
+       print(number)
+
+
+# In[12]:
+
+
+list2 = [12, 14, -95, 3]
+
+for number in list2:
+    if number>0:
+        print(number)
+
+
+# In[ ]:
+
+
+
+
